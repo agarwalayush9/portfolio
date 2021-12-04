006844db8eb1cceef256e9edc5f90562b183c533
agarwalayush9 <agarwalayush991@gmail.com>
2021-12-05 00:01:21 +0530

    nav changes

--- portfolio.js
+++ portfolio.js
@@ -5,3 +5,29 @@ op.addEventListener("click", ()=>{
     open.style.display="block";
     op.style.display="none";
 })
+let width= screen.width;
+let close= document.getElementById("close");
+let show= document.getElementById("show");
+let blur=document.getElementById("blur");
+let nav=  document.getElementById("navHid");
+close.style.display= "none";
+if(width <= 500){
+    show.style.display="block";
+    nav.style.display="none";
+}
+else{
+    nav.style.display="block";
+    show.style.display= "none";
+}
+show.addEventListener('click',()=>{
+    nav.style.display="block";
+    show.style.display= "none";
+    close.style.display= "block";
+    blur.style.filter="blur(1px)"
+})
+close.addEventListener("click",()=>{
+    nav.style.display="none";
+    show.style.display= "block";
+    close.style.display= "none";
+    blur.style.filter="blur(0px)"
+})
